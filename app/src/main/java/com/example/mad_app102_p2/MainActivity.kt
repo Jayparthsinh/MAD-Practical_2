f5e0965ccffb22d9554a2140c1b0fa6c0c7f6c6f
Jayparthsinh <150107887+Jayparthsinh@users.noreply.github.com>
2023-11-07 12:53:06 +0530

    final

---
+++ app/src/main/java/com/example/mad_app102_p2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mad_app102_p2
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    override fun onStart() {
+        super.onStart()
+        displayMsg("Onstart Method called")
+    }
+    override fun onResume() {
+        super.onResume()
+        displayMsg("OnResume Method called")
+    }
+    override fun onPause() {
+        super.onPause()
+        displayMsg("onPause Method called")
+    }
+    override fun onStop() {
+        super.onStop()
+        displayMsg("onStop Method called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        displayMsg("onDestroy Method called")
+    }
+    fun displayMsg(msg:String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.mycl), msg,Snackbar.LENGTH_SHORT).show()
+        Log.i("MainActivity", msg)
+    }
+}
